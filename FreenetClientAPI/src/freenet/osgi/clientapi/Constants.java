1387f2691bb8d1e6b5cf3c7bf35802e2d6942371
saces <saces@freenetproject.org>
2010-07-16 17:27:07 +0200

    add bundle FreenetClientAPI
    This is the new client api for foe.
    The API have two implementations, one provided by FreenetService
    itself (backed with native java calls), the other is backed by FCP.
    So your application can act as node plugin or remote client without
    changes.
    Just request an implementation from the framework, you get a working
    one. ;)

---
+++ FreenetClientAPI/src/freenet/osgi/clientapi/Constants.java
@@ -0,0 +1,11 @@
+package freenet.osgi.clientapi;
+
+public class Constants {
+
+	private Constants () {}
+
+	public static final int CLIENT_CHK_EXTRA_LENGTH = 5;
+
+	public static final int CLIENT_SSK_EXTRA_LENGTH = 5;
+
+}
