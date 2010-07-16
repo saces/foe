447ba03be63115854099aaff1e2819d424136cfc
saces <saces@freenetproject.org>
2010-07-16 17:27:49 +0200

    FreenetLogger: comment

--- FreenetLogger/src/freenet/log/Closer.java
+++ FreenetLogger/src/freenet/log/Closer.java
@@ -3,6 +3,7 @@ package freenet.log;
 import java.io.Closeable;
 import java.io.IOException;
 
+// This is here to avoid cyclic dependencies with FreenetSupport
 class Closer {
 
 	static void close(Closeable c) {
